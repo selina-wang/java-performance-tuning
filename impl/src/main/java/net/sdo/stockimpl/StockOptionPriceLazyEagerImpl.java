3f0dca5dd18c5b54cbe3dd54708ec9524e5857d6
Chen Huimin <349196770@qq.com>
2016-03-15 10:04:53 +0800

    first commit

---
+++ impl/src/main/java/net/sdo/stockimpl/StockOptionPriceLazyEagerImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2013,2014 Scott Oaks. All rights reserved.
+ */
+
+package net.sdo.stockimpl;
+
+import net.sdo.stock.StockOptionPrice;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "StockOptionPrice")
+public class StockOptionPriceLazyEagerImpl implements StockOptionPrice {
+
+    @EmbeddedId
+    private StockOptionPK id;
+    @Column(precision = 30, scale = 2)
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumns({
+        @JoinColumn(name = "symbol", referencedColumnName = "SYMBOL",
+                    insertable = false, updatable = false),
+        @JoinColumn(name = "priceDate", referencedColumnName = "priceDate",
+                    insertable = false, updatable = false)
+    })
+    private StockPriceLazyEagerImpl stock;
+
+    @Override
+    public String getSymbol() {
+        return id.getSymbol();
+    }
+
+    @Override
+    public Date getDate() {
+        return id.getDate();
+    }
+
+    @Override
+    public int getExpirationPeriod() {
+        return id.getExpirationPeriod();
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setId(StockOptionPK id) {
+        this.id = id;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
